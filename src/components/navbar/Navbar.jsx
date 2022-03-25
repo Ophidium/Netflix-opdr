1fe450776e9833165c55ae545fea6b4e946fd9dd
BretsW <59100941+Ophidium@users.noreply.github.com>
2022-03-25 14:36:59 +0100

    completed navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,17 +2,21 @@
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import SearchIcon from '@mui/icons-material/Search';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { useState } from "react";
 import "./navbar.scss"
 
 const Navbar = () => {
-    // const [isScrolled, setIsScrolled] = useState(false)
-
-    // console.log(window.pageYOffset)
+    const [isScrolled, setIsScrolled] = useState(false);
+    window.onscroll = () => {
+        setIsScrolled(window.pageYOffset === 0 ? false : true);
+        return () => (window.onscroll = null);
+    };
     return (
-        <div className='navbar'>
+        <div className={isScrolled ? "navbar scrolled" : "navbar"}>
             <div className="container">
                 <div className="left">
                     <img
+                        // might change it with a new logo 
                         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
                         alt=""
                     />
